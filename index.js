c31199abecd4fb002bcbbd680f36e4af9f65a6ea
Rafael Prado <rafael_adcp@hotmail.com>
2022-01-16 12:19:50 -0300

    creating a getEstimative() to allow folks to custom log what they want, instead of the auto built one

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const _ = require("lodash");
 const luxon = require("luxon");
 
 const luxonDateTime = luxon.DateTime;
-const DATE_FORMAT_FULL  = 'yyyy-MM-dd hh:mm:ss.SSS ZZZZ';
+const DATE_FORMAT_FULL = "yyyy-MM-dd hh:mm:ss.SSS ZZZZ";
 
 module.exports = class ProgressFollowup {
   constructor(totalAmountOfIterations, logEvery = 10) {
@@ -19,43 +19,61 @@ module.exports = class ProgressFollowup {
     this.checkpoints.push(Date.now());
   }
 
-  estimate() {
+  getEstimative() {
     /*
         need to subtract 1 here, since the first entry is the start point
         this is to allow us to have metrics for the first iteration,
         otherwise we would need 2 iterations to get the first metric
-        */
+    */
     const iterationsSoFar = _.size(this.checkpoints) - 1;
 
-    if (iterationsSoFar % this.logEvery == 0) {
-      const startTime = _.head(this.checkpoints);
-      const lastTime = _.last(this.checkpoints);
+    const startTime = _.head(this.checkpoints);
+    const lastTime = _.last(this.checkpoints);
 
-      const elapsedTimeInMS = lastTime - startTime;
+    const elapsedTimeInMS = lastTime - startTime;
 
-      const velocity = _.round(elapsedTimeInMS / iterationsSoFar, 2);
+    const velocity = _.round(elapsedTimeInMS / iterationsSoFar, 2);
 
-      const remainingToProcess = this.totalAmountOfIterations - iterationsSoFar;
+    const remainingToProcess = this.totalAmountOfIterations - iterationsSoFar;
 
-      const estimated = (elapsedTimeInMS * remainingToProcess) / iterationsSoFar;
+    const estimated = (elapsedTimeInMS * remainingToProcess) / iterationsSoFar;
 
-      const finishDate = luxonDateTime
-        .fromMillis(Date.now() + estimated)
-        .toFormat(DATE_FORMAT_FULL);
+    const finishDate = luxonDateTime
+      .fromMillis(Date.now() + estimated)
+      .toFormat(DATE_FORMAT_FULL);
 
-      const remainingTime = luxon.Duration.fromObject({
-        milliseconds: estimated,
-      }).toFormat("hh:mm:ss.SSS");
+    const remainingTime = luxon.Duration.fromObject({
+      milliseconds: estimated,
+    }).toFormat("hh:mm:ss.SSS");
 
-      const elapsedtimePretty = luxon.Duration.fromObject({
-        milliseconds: elapsedTimeInMS,
-      }).toFormat("hh:mm:ss.SSS");
+    const elapsedtimePretty = luxon.Duration.fromObject({
+      milliseconds: elapsedTimeInMS,
+    }).toFormat("hh:mm:ss.SSS");
 
+    return {
+      iterationsSoFar,
+      startTime,
+      lastTime,
+      elapsedTimeInMS,
+      velocity,
+      remainingToProcess,
+      estimated,
+      finishDate,
+      remainingTime,
+      elapsedtimePretty,
+    };
+  }
 
+  estimate() {
+    const estimative = this.getEstimative();
+
+    if (estimative.iterationsSoFar % this.logEvery == 0) {
       console.log(
-        `${luxonDateTime.now().toFormat(DATE_FORMAT_FULL)}: [${iterationsSoFar}/${this.totalAmountOfIterations}] ` +
-        `elapsed time [${elapsedtimePretty}], velocity [${velocity} ms/iteration] ` +
-        `remaining [${remainingTime}] ====> ETA at ${finishDate}`
+        `${luxonDateTime.now().toFormat(DATE_FORMAT_FULL)}: ` +
+          `[${estimative.iterationsSoFar}/${this.totalAmountOfIterations}] ` +
+          `elapsed time [${estimative.elapsedtimePretty}], ` +
+          `velocity [${estimative.velocity} ms/iteration], ` +
+          `remaining [${estimative.remainingTime}] ====> ETA at ${estimative.finishDate}`
       );
     }
   }
